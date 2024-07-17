13ab953dcdff26df3ed1c00bdb4c66068773b87a
FrankTIII <frank_training@outlook.com>
2024-07-16 18:55:04 -0600

    1st commint: Connect2 Repo

---
+++ src/main/java/com/allura/LibreriaDB/LibreriaDbApplication.java
@@ -0,0 +1,13 @@
+package com.allura.LibreriaDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaDbApplication.class, args);
+	}
+
+}
